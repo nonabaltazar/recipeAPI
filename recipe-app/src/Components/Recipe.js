013d7adc159b9edfcb8084eb8c694aeeb1930f04
nonabaltazar <rafaeladbaltazar@gmail.com>
2018-08-15 06:18:04 -0700

    added routers

---
+++ recipe-app/src/Components/Recipe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class Recipe extends React.Component {
+    render(){
+        return (
+            <div>Recipe Component</div>
+        )
+    }
+}
+
+export default Recipe;
--- recipe-app/src/Components/Recipes.js
+++ recipe-app/src/Components/Recipes.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Recipes = (props) => {
     return(
-<div>
+<div className="container">
+<div className="row">
 { props.recipes.map((recipe) => {
-    return (<div key={recipe.recipe_id}>
-      <img src={recipe.image_url} alt={recipe.title}/>
-      <p>{ recipe.title }</p></div>)
- }) }
+    return (
+    <div key={recipe.title} className="col-md-4" style={{marginBottom: "2rem"}}>
+        <div className="recipes__box">
+            <img 
+            className="recipes__box-img"
+            src={recipe.image_url} 
+            alt={recipe.title} 
+            />
+            <div className="recipes__text">
+            <h5 className="recipes__title">
+            { recipe.title.length < 20 ? `${ recipe.title }` :`${recipe.title.substring(0,25)}...` }
+             </h5>
+            <p className="recipes__subtitle">
+            Publisher:
+            <span>{recipe.publisher}</span>
+            </p>
+            </div>
+            <button className="recipe_buttons">
+            
+            <Link to=
+            {{
+                pathname: `/recipe/${recipe.recipe_id}`,
+                state: { recipe: recipe.title}
+            
+            }}>
+            
+            View Recipe</Link>
+            
+            </button>
+        </div>
+    </div>
+    )})}
+</div>
 
 </div>
 
@@ -15,5 +46,5 @@ const Recipes = (props) => {
     )
 }
 
-export default Recipes
+export default Recipes;
 
